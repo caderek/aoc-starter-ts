614f6f1f52bd070d68ba7f16a3b97cb4762cc19c
Maciej Cąderek <maciej.caderek@gmail.com>
2019-12-02 20:46:45 +0100

    Used shelljs to support Windows

--- scripts/start.js
+++ scripts/start.js
@@ -1,12 +1,13 @@
 const { spawn, execSync } = require("child_process")
 const { readdirSync } = require("fs")
+const { cp } = require("shelljs")
 
 const day = process.argv[2]
 const days = readdirSync("./src")
 
 if (!days.includes(day)) {
   console.log(`Creating file structure for ${day}...`)
-  execSync(`cp -r src/template src/${day}`)
+  cp("-r", "src/template", `src/${day}`)
 }
 
 spawn("nodemon", ["-x", "ts-node", `src/${day}/index.ts`], {
