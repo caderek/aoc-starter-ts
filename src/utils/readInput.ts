9b121e9c9bc1e5a4ae35d578ad71dfd8863d98cd
Maor Leger <maor.leger@gmail.com>
2020-12-01 00:51:53 +0000

    Support windows

--- src/utils/readInput.ts
+++ src/utils/readInput.ts
@@ -1,12 +1,12 @@
 import { readFileSync } from "fs"
 import * as getCallerFile from "get-caller-file"
+import * as path from "path"
 
 export const readInput = () => {
-  const file = getCallerFile()
-    .split("/")
-    .slice(0, -1)
-    .concat("input.txt")
-    .join("/")
+  const file = path.join(
+    path.dirname(getCallerFile()),
+    "input.txt"
+  );
 
   return readFileSync(file).toString()
 }
